91b97eddea029ac01d5a231e2b9a52044c968500
V. V. S. Ravi Sankar <23981a05ae@raghuenggcollege.in>
2026-03-30 23:50:50 +0530

    sardarad 2

--- core/settings.py
+++ core/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
@@ -132,6 +132,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOWED_ORIGINS = [
+    "https://zaarava-frontend.vercel.app",
+]
+import os
+
+# Base url to serve media files
 import os
 
 MEDIA_URL = '/media/'
@@ -149,19 +163,7 @@ STORAGES = {
 # This tells Cloudinary to look for the key in Render's environment
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
-} 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = [
-    "https://zaarava-frontend.vercel.app",
-]
-import os
-
-# Base url to serve media files
-MEDIA_URL = '/media/'
+}
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
