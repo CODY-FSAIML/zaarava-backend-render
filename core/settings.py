344aeb8faacbf2b12f762bd2058f1522e310928d
V. V. S. Ravi Sankar <23981a05ae@raghuenggcollege.in>
2026-03-30 23:03:28 +0530

    production ready

--- core/settings.py
+++ core/settings.py
@@ -140,10 +140,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://localhost:8081",
-    "http://127.0.0.1:8081",
+    "https://zaarava-frontend.vercel.app/",
 ]
 import os
 
