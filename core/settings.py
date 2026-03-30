1247eac1efcc53ce9a453fa3789321f1a6f65e7d
V. V. S. Ravi Sankar <23981a05ae@raghuenggcollege.in>
2026-03-30 23:47:22 +0530

    sardarad

--- core/settings.py
+++ core/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
+    'cloudinary',
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
