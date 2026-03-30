dbda1b34cd36df78ef2326057c5d8c52df0cb0e0
V. V. S. Ravi Sankar <23981a05ae@raghuenggcollege.in>
2026-03-30 23:10:44 +0530

    production ready for superuser

--- build.sh
+++ build.sh
@@ -5,3 +5,14 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# NEW: Automatically create superuser using Render's environment variables
+# The "|| true" prevents the build from crashing if the user already exists!
+python manage.py createsuperuser --noinput || true
